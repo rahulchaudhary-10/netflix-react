d94c3ac6d6b913bec9f2a9b26c405a810d6f15a3
Rahul Chaudhary <110348098+rahulchaudhary-10@users.noreply.github.com>
2023-04-24 17:37:49 +0530

    s

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import logo from "../../logo.png"
-import { Link } from 'react-router-dom'
-import {ImSearch} from "react-icons/im"
+import { Link } from "react-router-dom"
+import { ImSearch } from "react-icons/im"
 
 const Header = () => {
-  return (
-   <nav className="header">
+    return (
+        <nav className="header">
 
-    <img src={logo}alt="logo" />
-    <div>
-        <Link to="/tvshows">TV Shows</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/recent">Recently Added</Link>
-        <Link to="/mylist">My List</Link>
-    </div>
-    <ImSearch />
-   </nav>
-  )
+            <img src={logo} alt="logo" />
+
+            <div>
+                <Link to="/tvshows" >TV Shows</Link>
+                <Link to="/movies" >Movies</Link>
+                <Link to="/recent" >Recently Added</Link>
+                <Link to="/mylist" >My List</Link>
+            </div>
+
+            <ImSearch />
+
+        </nav>
+    )
 }
 
 export default Header
